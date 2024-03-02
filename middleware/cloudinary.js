2c04b71452d9f691ecfb12a2d9d7e5181ace70db
Yudi Runat Masneno <yudi.berland@gmail.com>
2024-03-02 20:01:03 +0700

    datas

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -1,14 +1,12 @@
 /* eslint-disable no-undef */
 import cloudinary from "cloudinary";
 const dataFuncCloudinary = cloudinary.v2;
-import dotenv from "dotenv";
-dotenv.config();
 
 // Konfigurasi Cloudinary
 await dataFuncCloudinary.config({
   cloud_name: "dymv3cmhq",
-  api_key: process?.env?.API_KEY_CLOUDINARY,
-  api_secret: process?.env?.API_SECRET_CLOUDINARY,
+  api_key: "668146521133731",
+  api_secret: "XgsP4ZTsA1Zn0Ja6KjuhQwZ5piE",
 });
 
 export default dataFuncCloudinary;
